613930e9b6900bb166b082f0c48e8213d1b0d202
Yuvraj Azad <yuvrajjsr253@gmail.com>
2026-09-02 16:32:36 +0530

    Allow Vercel frontend in CORS

--- backend n/backend/main.py
+++ backend n/backend/main.py
@@ -32,7 +32,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://orca-marine-ecosystem.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
